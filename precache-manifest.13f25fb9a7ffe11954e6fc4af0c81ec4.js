230b36908a75eeeb1c8cd9079365ddb87afc2bed
abdullah <hbkamk@gmail.com>
2020-07-17 18:47:00 +0600

    Updates

---
+++ precache-manifest.13f25fb9a7ffe11954e6fc4af0c81ec4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee55e25e602b688ea200d1f4c7ba057",
+    "url": "/Recipe-Search-Application/index.html"
+  },
+  {
+    "revision": "28753332abecc9277cde",
+    "url": "/Recipe-Search-Application/static/css/main.b5320ed5.chunk.css"
+  },
+  {
+    "revision": "5725ad80a98df43259e2",
+    "url": "/Recipe-Search-Application/static/js/2.87fdde3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-Search-Application/static/js/2.87fdde3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28753332abecc9277cde",
+    "url": "/Recipe-Search-Application/static/js/main.b2745055.chunk.js"
+  },
+  {
+    "revision": "afd8a84842c6f339c557",
+    "url": "/Recipe-Search-Application/static/js/runtime-main.6ce44f2d.js"
+  }
+]);
